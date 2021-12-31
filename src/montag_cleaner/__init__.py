da9d6b3897f1dcf1fa9de3f27d131c0344099c35
SG <13872653+mmguero@users.noreply.github.com>
2021-12-31 10:29:54 -0700

    v1.0.1 release

--- src/montag_cleaner/__init__.py
+++ src/montag_cleaner/__init__.py
@@ -1,6 +1,6 @@
 """Montag is a utility which reads e-book files and scrubs them of profanity."""
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __author__ = "Seth Grover <mero.mero.guero@gmail.com>"
 __all__ = []
 
