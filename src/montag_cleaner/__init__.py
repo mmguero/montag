a0c4997923179863a8b379366c7c5f856a6cbc3c
Seth Grover <mero.mero.guero@gmail.com>
2022-11-28 15:54:39 -0700

    minor tweak to defaults

--- src/montag_cleaner/__init__.py
+++ src/montag_cleaner/__init__.py
@@ -1,6 +1,6 @@
 """Montag is a utility which reads e-book files and scrubs them of profanity."""
 
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 __author__ = "Seth Grover <mero.mero.guero@gmail.com>"
 __all__ = []
 
